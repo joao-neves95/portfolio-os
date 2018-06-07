4a45f3185ca6bee65e9f66bbaa33a8e8da52dcf9
shivayl <joao95neves@gmail.com>
2018-06-07 23:36:46 +0100

    Set up basic folder structure.

---
+++ client/private/js/src/index.js
@@ -0,0 +1,11 @@
+﻿// Imports for MergerJS.
+//
+// @import './main'
+// @import './utils'
+// @import './services/networking'
+// @import './services/windowManager/windowManager.html'
+// @import './services/windowManager/windowManager.handlers'
+// @import './components/calculator/calculator.html'
+// @import './components/calculator/calculator.handlers'
+// @import './components/calculator/calculator.listeners'
+//
---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+const path = require('path');
+const express = require('express');
+const routes = require('./server/routes/index.js');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve public files.
+app.use('/', express.static(path.join(__dirname, './client/wwwroot')));
+
+// TODO: Add database connection.
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.info(`The server is listening on port ${PORT}`);
+});
